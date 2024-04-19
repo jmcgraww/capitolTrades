eaf165a8bbc9c1c152187f2c202a36737b38a394
jmcgraww <127436695+jmcgraww@users.noreply.github.com>
2024-04-19 18:47:54 -0400

    Added auto-scrolling when selecting a result
    Also tried to add coloring of stock ticker buttons, was unsuccessful.  there is some code related to it in CongressTrades.js

--- src/components/pages/CongressTrades.js
+++ src/components/pages/CongressTrades.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
 
 const CongressTrades = () => {
@@ -6,22 +6,26 @@ const CongressTrades = () => {
   const [congressMember, setCongressMember] = useState('');
   const [trades, setTrades] = useState([]);
   const [visibleTrades, setVisibleTrades] = useState({});
+  const [prevVisibleTrades, setPrevVisibleTrades] = useState({});
   const [error, setError] = useState('');
-  const [dataStructure, setDataStructure] = useState(null); // State to track the chosen data structure
+  const [dataStructure, setDataStructure] = useState(null);
   const [searchBarVisible, setSearchBarVisible] = useState(false);
   const [queryTime, setQueryTime] = useState(null);
+  const [expandedTicker, setExpandedTicker] = useState(null); // Track the currently expanded ticker
+  const expandedRef = useRef(null);
+
+  useEffect(() => {
+    scrollToExpandedInfo();
+  }, [visibleTrades]);
 
   const handleSearch = async () => {
-    console.log('Initiating search for:', inputValue);
     try {
-      const startTime = Date.now(); // Record start time
+      const startTime = performance.now(); // Start measuring time
       const response = await axios.get('/search', {
-        params: { term: inputValue, data_structure: dataStructure }, // Pass chosen data structure
+        params: { term: inputValue, data_structure: dataStructure },
       });
-      const endTime = Date.now(); // Record end time
-      const timeElapsed = endTime - startTime; // Calculate time taken for the query
-      console.log('Search results:', response.data);
-      setQueryTime(timeElapsed); // Set query time
+      const endTime = performance.now(); // End measuring time
+      setQueryTime((endTime - startTime).toFixed(2)); // Calculate and set query time
       if (response.data.length > 0) {
         setSearchBarVisible(true);
         fetchTrades(response.data[0]);
@@ -31,56 +35,89 @@ const CongressTrades = () => {
         setCongressMember('');
       }
     } catch (error) {
-      console.error('Error fetching matches:', error);
       setError('Error fetching data. Please try again later.');
       setTrades([]);
       setCongressMember('');
     }
   };
+  
 
   const fetchTrades = async (name) => {
-    console.log('Fetching trades for:', name);
     try {
       const response = await axios.get(`/trades/${name}`, {
-        params: { data_structure: dataStructure }, // Pass chosen data structure
+        params: { data_structure: dataStructure },
       });
-      console.log('Trades fetched:', response.data);
       setTrades(response.data);
       setCongressMember(name);
       setError('');
     } catch (error) {
-      console.error('Error fetching trades:', error);
       setError('Error fetching trades. Please try again later.');
       setTrades([]);
     }
   };
 
   const toggleTradeVisibility = (ticker) => {
-    setVisibleTrades((prevVisibleTrades) => {
-      const newVisibleTrades = {
-        ...prevVisibleTrades,
-        [ticker]: !prevVisibleTrades[ticker],
-      };
-      console.log('Visibility Toggled for:', ticker, newVisibleTrades[ticker]);
-      return newVisibleTrades;
+    setVisibleTrades((prevVisibleTrades) => ({
+      ...prevVisibleTrades,
+      [ticker]: !prevVisibleTrades[ticker],
+    }));
+    setExpandedTicker(ticker); // Update the expanded ticker
+  };
+
+  const scrollToExpandedInfo = () => {
+    Object.keys(visibleTrades).forEach((ticker) => {
+      // Check if trade visibility changed from false to true
+      if (!prevVisibleTrades[ticker] && visibleTrades[ticker]) {
+        const buttonRef = document.getElementById(`button-${ticker}`);
+        if (buttonRef && expandedRef.current) {
+          const searchBarRect = document.getElementById('search-bar').getBoundingClientRect();
+          const buttonRect = buttonRef.getBoundingClientRect();
+          const windowHeight = window.innerHeight;
+          const buttonTop = buttonRect.top - searchBarRect.height - windowHeight / 10;
+          // Check if the button is already above the current viewport
+          if (buttonTop > window.pageYOffset) {
+            window.scrollTo({
+              top: buttonTop,
+              behavior: 'smooth',
+            });
+          }
+        }
+      }
     });
+    // Update previous visibility state
+    setPrevVisibleTrades(visibleTrades);
+  };
+
+  const getLastTradeType = (ticker) => {
+    const lastTrade = trades[ticker] && trades[ticker].length > 0 ? trades[ticker][0].trade_type : null;
+    return lastTrade;
+  };
+
+  const getButtonColor = (ticker) => {
+    const lastTradeType = getLastTradeType(ticker);
+    if (lastTradeType === 'Purchase') {
+      return 'green'; // Green color for purchase
+    } else if (lastTradeType === 'Sale') {
+      return 'red'; // Red color for sale
+    } else {
+      return '#efefef'; // Default color
+    }
   };
 
   return (
     <div>
-      <h1 style={{ marginBottom: '30px' }}>Capitol Trades</h1>
+      <h1 style={{ marginTop: '60px', marginBottom: '30px' }}>Capitol Trades</h1>
       <div style={{ textAlign: 'center', marginBottom: '20px' }}>
         {!searchBarVisible && (
           <>
             <h2 style={{ marginBottom: '20px' }}>Would you like to use an Adjacency List or Adjacency Matrix in the backend?</h2>
             <div>
-            <button style={{ fontSize: '20px', padding: '10px 20px', margin: '10px' }} onClick={() => { setDataStructure('list'); setSearchBarVisible(true); }}>
-              Use Adjacency List
-            </button>
-            <button style={{ fontSize: '20px', padding: '10px 20px', margin: '10px' }} onClick={() => { setDataStructure('matrix'); setSearchBarVisible(true); }}>
-              Use Adjacency Matrix
-            </button>
-
+              <button style={{ fontSize: '20px', padding: '10px 20px', margin: '10px', marginBottom: '10px' }} onClick={() => { setDataStructure('list'); setSearchBarVisible(true); }}>
+                Use Adjacency List
+              </button>
+              <button style={{ fontSize: '20px', padding: '10px 20px', margin: '10px', marginBottom: '10px' }} onClick={() => { setDataStructure('matrix'); setSearchBarVisible(true); }}>
+                Use Adjacency Matrix
+              </button>
             </div>
           </>
         )}
@@ -88,6 +125,7 @@ const CongressTrades = () => {
           <>
             <h2 style={{ marginBottom: '20px' }}>Enter a congress member:</h2>
             <input
+              id="search-bar"
               type="text"
               value={inputValue}
               onChange={(e) => setInputValue(e.target.value)}
@@ -101,12 +139,12 @@ const CongressTrades = () => {
               }}
             />
             <br />
-            <button onClick={handleSearch} style={{ fontSize: '20px', padding: '10px 20px', margin: '10px' }}>
-              Search
-            </button>
             {queryTime && (
               <p style={{ fontSize: '16px', marginTop: '10px' }}>Query Time: {queryTime} ms</p>
             )}
+            <button onClick={handleSearch} style={{ fontSize: '20px', padding: '10px 20px', margin: '10px', marginBottom: '20px' }}>
+              Search
+            </button>
           </>
         )}
       </div>
@@ -118,80 +156,81 @@ const CongressTrades = () => {
       <div
         style={{
           display: 'grid',
-          gridTemplateColumns: 'repeat(3, 1fr)', // Adjusted minmax width
+          gridTemplateColumns: 'repeat(3, 1fr)',
           gap: '20px',
           marginLeft: '20px',
           marginRight: '20px',
         }}
       >
-        {Object.keys(trades).length > 0 &&
-          Object.entries(trades).map(([ticker, tradeDetails], index) => (
-            <div
-              key={`${ticker}-${visibleTrades[ticker]}`}
+        {Object.keys(trades).map((ticker) => (
+          <div
+            key={`${ticker}-${visibleTrades[ticker]}`}
+            style={{
+              position: 'relative',
+              marginBottom: '20px',
+            }}
+          >
+            <button
+              id={`button-${ticker}`}
+              onClick={() => toggleTradeVisibility(ticker)}
               style={{
-                position: 'relative',
-                overflow: 'visible',
+                width: '100%',
+                height: '60px',
+                fontSize: '16px',
+                borderRadius: '5px',
+                background: getButtonColor(ticker), // Set button color dynamically
+                border: 'none',
+                boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.2)',
+                cursor: 'pointer',
+                transition: 'transform 0.3s',
+                overflow: 'hidden',
+                whiteSpace: 'nowrap',
+                textOverflow: 'ellipsis',
+              }}
+              onMouseEnter={(e) => {
+                e.target.style.background = '#d9d9d9';
+              }}
+              onMouseLeave={(e) => {
+                e.target.style.background = getButtonColor(ticker); // Restore original color on mouse leave
               }}
             >
-              <button
-                onClick={() => toggleTradeVisibility(ticker)}
+              {ticker}
+            </button>
+            {visibleTrades[ticker] && (
+              <div
+                ref={expandedRef}
                 style={{
-                  width: '100%', // Adjusted width
-                  height: '60px',
-                  fontSize: '16px',
+                  position: 'absolute',
+                  top: 'calc(100% + 5px)',
+                  left: '0',
+                  right: '0',
+                  background: '#ffffff',
+                  boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.25)',
+                  padding: '15px',
                   borderRadius: '5px',
-                  background: '#efefef',
-                  border: 'none',
-                  boxShadow: '0px 2px 5px rgba(0, 0, 0, 0.2)',
-                  cursor: 'pointer',
-                  transition: 'transform 0.3s',
-                  overflow: 'hidden',
-                  whiteSpace: 'nowrap',
-                  textOverflow: 'ellipsis',
-                }}
-                onMouseEnter={(e) => {
-                  e.target.style.background = '#d9d9d9';
-                }}
-                onMouseLeave={(e) => {
-                  e.target.style.background = '#efefef';
+                  zIndex: '10',
                 }}
               >
-                {ticker}
-              </button>
-              {visibleTrades[ticker] && (
-                <div
-                  style={{
-                    position: 'absolute',
-                    top: 'calc(100% + 5px)',
-                    left: '0',
-                    right: '0',
-                    background: '#ffffff',
-                    boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.25)',
-                    padding: '15px',
-                    borderRadius: '5px',
-                    zIndex: '10',
-                  }}
-                >
-                  {tradeDetails.length > 0 && (
-                    <strong>{tradeDetails[0].name.slice(0, 20)}</strong>
-                  )}
-                  <ul>
-                    {tradeDetails.map((trade, idx) => (
-                      <li
-                        key={idx}
-                        style={{ background: '#f7f7f7', margin: '5px 0', padding: '10px', borderRadius: '5px' }}
-                      >
-                        <div>
-                          Date: {trade.date} | Type: {trade.trade_type} | {trade.amount.toLocaleString()}
-                          {/* Formatting number with commas */}
-                        </div>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              )}
-            </div>
-          ))}
+                {trades[ticker]
+                  .sort((a, b) => {
+                    const dateA = new Date(a.date);
+                    const dateB = new Date(b.date);
+                    return dateB - dateA; // Sort in descending order
+                  })
+                  .map((trade, idx) => (
+                    <li
+                      key={idx}
+                      style={{ background: '#f7f7f7', margin: '5px 0', padding: '10px', borderRadius: '5px' }}
+                    >
+                      <div>
+                        Date: {trade.date} | Type: {trade.trade_type} | {trade.amount.toLocaleString()}
+                      </div>
+                    </li>
+                  ))}
+              </div>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
